f999b3335926cfe3787856d3356c0a7dc7db22a6
Aidan Handa <akhanda@stu.naperville203.org>
2019-10-26 06:43:38 -0400

    Old Navy :)

--- OldNavy.py
+++ OldNavy.py
@@ -1,7 +1,10 @@
 import time
 
 import bs4
-from Helper import get_webdriver
+from selenium.webdriver.common.by import By
+
+from Helper import get_webdriver, waitForLoad
+
 
 #TODO: NOT WORKING
 def search_oldnavy(*keywords):
@@ -13,12 +16,26 @@ def search_oldnavy(*keywords):
     URL = "https://oldnavy.gap.com/browse/search.do?searchText="
     KEYWORD_STRING = "%20".join(keywords)
     URL += KEYWORD_STRING
+    dataList = []
     wd = get_webdriver()
     wd.get(URL)
-    with open("out.html","w") as file:
-        file.write(wd.page_source)
+    waitForLoad(URL, wd, "product-card-grid__inner", By.CLASS_NAME)
     web_page = bs4.BeautifulSoup(wd.page_source, "lxml")
-    print(web_page.head.title.text)
     articles = web_page.find_all("div", class_="product-card-grid__inner")
     wd.close()
-    return articles
+    for item in articles:
+        data = {}
+        try:
+            data["name"] = item.find_all("div", class_="product-card__name")[0].text.strip()
+            highlight_prices = item.find_all(class_="product-price__highlight")
+            if len(highlight_prices) > 0:
+                data["price"] = highlight_prices[0].text.strip()
+            else:
+                data["price"] = item.find_all("div", class_="product-card-price")[0].text.strip()
+            data["image"] = "https://oldnavy.gap.com" + item.find_all("img",class_="product-card__image")[0]["src"]
+            data["url"] = item.find_all("a", class_="product-card__link")[0]["href"]
+        except:
+            continue
+        dataList.append(data)
+
+    return dataList
--- main.py
+++ main.py
@@ -3,5 +3,13 @@ from Kohls import search_kohls
 from Nordstrom import search_nordstrom
 from OldNavy import search_oldnavy
 
-for i in search_bloomingdales("red","jeans"):
-    print(i)
+keywords = ("blue", "dress")
+def main():
+    for entry in search_kohls(*keywords) \
+                 + search_nordstrom(*keywords) \
+                 + search_bloomingdales(*keywords) \
+                 + search_oldnavy(*keywords):
+        print(entry)
+
+if __name__ == "__main__":
+    main()
